ba7f7f0e9d5a15e37d3c204445fc01e16ce74eb2
Olga <kindzyadzyu@gmail.com>
2020-06-24 08:09:10 +0300

    refactoring

--- Cocktail DB/Cocktail DB/Extensions/UITableView + hideSeparator.swift
+++ Cocktail DB/Cocktail DB/Extensions/UITableView + hideSeparator.swift
@@ -1,13 +1,14 @@
 //
-//  UITabloeView + hideSeparator.swift
+//  UITableView + HideSeparator.swift
 //  Cocktail DB
 //
-//  Created by Olha Bereziuk on 22.06.2020.
+//  Created by Olha Bereziuk on 24.06.2020.
 //  Copyright © 2020 Olha Bereziuk. All rights reserved.
 //
 
 import UIKit
 
+//Hide cell's separator line
 extension UITableView {
     func removeSeparator() {
         self.separatorStyle = .none
